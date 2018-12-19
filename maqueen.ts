3db6365692fa9927c4e18433764c9d172b2d382e
jhlucky <460221531@qq.com>
2018-12-19 15:11:00 +0800

    Update maqueen.ts

--- maqueen.ts
+++ maqueen.ts
@@ -121,7 +121,7 @@ namespace maqueen{
         //pins.digitalWritePin(DigitalPin.P1, 0);
         //control.waitMicros(2);
         pins.digitalWritePin(DigitalPin.P1, 1);
-        control.waitMicros(10);
+        //control.waitMicros(10);
         pins.digitalWritePin(DigitalPin.P1, 0);
 	    
 	//pins.setPull(DigitalPin.P2, PinPullMode.PullUp);
